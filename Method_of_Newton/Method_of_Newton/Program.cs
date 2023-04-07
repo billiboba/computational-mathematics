76d01d647d03ed53c0ca0e1ec7975dae5620ece7
Roma.Kor <romakor.19@yandex.ru>
2023-04-08 02:39:11 +0700

    Method of Neton

---
+++ Method_of_Newton/Method_of_Newton/Program.cs
@@ -0,0 +1,49 @@
+﻿using static System.Console;
+using static System.Math;
+class Program
+{
+    public const double eps = 0.01;
+
+    unsafe static double Equation(double x)
+    {
+        return x * x * x + 4 * x - 3;
+    }
+    static double Equation1(double x)
+    {
+        return 3 * x * x + 4;
+    }
+    static double Equation2(double x)
+    {
+        return 6 * x;
+    }
+    unsafe public static int Newton(double a, double b, double* c, double eps)
+    {
+        unsafe
+        {
+            int k = 0;
+            if (Equation(a) * Equation2(a) > 0) *c = a;
+            else *c = b;
+            do
+            {
+                *c = *c - Equation(*c) / Equation1(*c);
+                k++;
+            }
+            while (Math.Abs(Equation(*c)) >= eps);
+            return k;
+        }
+
+    }
+    unsafe static void Main(string[] args)
+    {
+        Console.WriteLine("Your function: x * x * x + 3 * x + 2.2");
+        int k;
+        double a, b, x;
+        Console.Write("a: ");
+        a = double.Parse(Console.ReadLine());
+        Console.Write("b: ");
+        b = double.Parse(Console.ReadLine());
+        Newton(a, b, &x, eps);
+        Console.Write("x: ");
+        Console.WriteLine(Math.Round(x, 3));
+    }
+}
