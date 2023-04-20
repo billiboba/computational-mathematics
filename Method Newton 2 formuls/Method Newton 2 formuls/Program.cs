cb5ab87e4b5bdd802beab8d7fa461cb783fba186
billiboba <romakor.19@yandex.ru>
2023-04-20 17:38:49 +0700

    Newton

---
+++ Method Newton 2 formuls/Method Newton 2 formuls/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+class GFG
+{
+    static double u_cal(double u, int n)
+    {
+        double temp = u;
+        for (int i = 1; i < n; i++)
+            temp = temp * (u + i);
+        return temp;
+    }
+    static int fact(int n)
+    {
+        int f = 1;
+        for (int i = 2; i <= n; i++)
+            f *= i;
+        return f;
+    }
+    static void Main()
+    {
+        int n = 5;
+        double[] x = { 1, 2, 3, 4, 5 };
+
+        double[,] y = new double[n, n];
+        y[0, 0] = 1.6;
+        y[1, 0] = 2.7;
+        y[2, 0] = 3.8;
+        y[3, 0] = 4.9;
+        y[4, 0] = 5.1;
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = n - 1; j >= i; j--)
+                y[j, i] = y[j, i - 1] - y[j - 1, i - 1];
+        }
+
+        double value = 4.6;
+        double sum = y[n - 1, 0];
+        double u = (value - x[n - 1]) / (x[1] - x[0]);
+        for (int i = 1; i < n; i++)
+        {
+            sum = sum + (u_cal(u, i) * y[n - 1, i]) /
+                                        fact(i);
+        }
+
+        Console.WriteLine("\n Value at " + value + " is " + Math.Round(sum, 4));
+    }
+}
---
+++ interpolation newton 1/interpolation newton 1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class GFG
+{
+    static double first_formul(double u, int n)
+    {
+        double temp = u;
+        for (int i = 1; i < n; i++)
+            temp = temp * (u - i);
+        return temp;
+    }
+
+    static int fact(int n)
+    {
+        int f = 1;
+        for (int i = 2; i <= n; i++)
+            f *= i;
+        return f;
+    }
+    public static void Main()
+    {
+        int n = 4;
+        double[] x = { 0.4, 0.8, 1.2, 1.6 };
+
+        double[,] y = new double[n, n];
+        y[0, 0] = 2;
+        y[1, 0] = 3.2;
+        y[2, 0] = 12;
+        y[3, 0] = 3;
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < n - i; j++)
+                y[j, i] = y[j + 1, i - 1] - y[j, i - 1];
+        }
+
+
+        double value = 1.3;
+
+        double sum = y[0, 0];
+        double u = (value - x[0]) / (x[1] - x[0]);
+        for (int i = 1; i < n; i++)
+        {
+            sum = sum + (first_formul(u, i) * y[0, i]) /
+                                    fact(i);
+        }
+
+        Console.WriteLine("\n Value at " + value + " is " + Math.Round(sum, 6));
+    }
+}
