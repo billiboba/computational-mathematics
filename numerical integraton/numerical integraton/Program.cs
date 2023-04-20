19af0c5f5c06b9065e98298c05a168cfb0433a97
Roma.Kor <romakor.19@yandex.ru>
2023-04-21 03:15:45 +0700

    integration

---
+++ numerical integraton/numerical integraton/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+using static System.Math;
+
+class integration
+{
+
+    static double Equation(double x)
+    {
+        return Math.Pow(x, 4) / (0.5 * Math.Pow(x, 2) + x + 6);
+    }
+
+    static double Method_of_Runge(double a, double b, double n)
+    {
+        double sum = 0.0;
+        double h = (b - a) / n;
+        for(int i = 0; i < n; i++)
+        {
+            sum += Equation(a + h * (i + 0.5));
+            sum *= h;
+        }
+        return sum;
+        
+    }
+    static void Main()
+    {
+        double a = 0.4;
+        double b = 1.5;
+        int n = 10;
+        Console.WriteLine(Method_of_Runge(a,b,n));
+        
+    }
+}
