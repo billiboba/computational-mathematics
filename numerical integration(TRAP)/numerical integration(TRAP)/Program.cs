9d19fbf321a124f903d76475877e6ba7098f5a47
Roma.Kor <romakor.19@yandex.ru>
2023-04-21 03:30:02 +0700

    numerical integration method of Trapezoid

---
+++ numerical integration(TRAP)/numerical integration(TRAP)/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+using static System.Math;
+
+class integration
+{
+
+    static double Equation(double x)
+    {
+        return Math.Pow(x, 4) / (0.5 * Math.Pow(x, 2) + x + 6);
+    }
+
+    static double Method_of_Trap(double a, double b, double n)
+    {
+        double sum = 0.0;
+        double h = (b - a) / n;
+        for (int i = 0; i < n; i++)
+        {
+            sum += 0.5 * h * (Equation(a + i * h) + Equation(a + (i + 1) * h));
+        }
+        return sum;
+
+    }
+    static void Main()
+    {
+        double a = 0.4;
+        double b = 1.5;
+        int n = 10;
+        Console.WriteLine(Method_of_Trap(a, b, n));
+
+    }
+}
