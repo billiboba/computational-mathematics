8e5101b9eb67c884aabebc0a75498d04fec7ef6d
Roma.Kor <romakor.19@yandex.ru>
2023-04-22 22:48:30 +0700

    Method of Euler

---
+++ Method_of_Euler/Method_of_Euler/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+using static System.Math;
+
+class Program
+{
+    static double Equation(double x, double y)
+    {
+        return  x+Math.Pow(y,2);
+    }
+
+    static void Method_of_Euler(double x0, double y, double x,double h)
+    {
+        double temp = 0.0;
+        while(x0 < x)
+        {
+            temp = y;
+            y = y+h*(Equation(x0,y));
+            x0 = x0+h;
+        }
+        Console.WriteLine("Solution at x = "+ x + " is y = " + y);
+    }
+
+    static void Main()
+    {
+        double x0 = 0;
+        double y= 1;
+        double x = 1.5;
+        double h = 0.5;
+        Method_of_Euler(x0 ,y,x,h);
+    }
+}
